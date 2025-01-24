0be813b6592426c95848b59943dab9e6fbfda7ed
Najeebkm010 <najeebkm010@gmail.com>
2025-01-24 11:26:34 +0400

    vercel

--- src/app.js
+++ src/app.js
@@ -33,6 +33,14 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+// Handle production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('public'));
+}
+
+module.exports = app;
